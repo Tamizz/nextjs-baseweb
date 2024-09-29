74dfd59672e85d1e44f5ffc15e2b2e5c5adc49a6
Andre Tamiazzo <andre.tamiazzo@gmail.com>
2024-09-29 13:23:52 -0700

    First project commit. Base components are working now. Added a HeaderNavigation component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import {Button} from 'baseui/button';
-import {useStyletron} from 'baseui';
+import { Button } from 'baseui/button';
+import { useStyletron } from 'baseui';
+import Header from '../components/Header';
 
 export const sum = (a: number, b: number) => a + b;
 
@@ -8,8 +9,11 @@ const Index: React.FC = () => {
   const [css, theme] = useStyletron();
   return (
     <div>
-      <Button onClick={() => console.log('hey')}>Hello</Button>
-      <p className={css({color: theme.colors.accent600})}>Styled by hook</p>
+      <Header />
+      <div className={css({ padding: '20px' })}>
+        <Button onClick={() => console.log('hey')}>Hello</Button>
+        <p className={css({color: theme.colors.accent600})}>Styled by hook</p>
+      </div>
     </div>
   );
 };
